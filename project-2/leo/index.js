51d164b3bb7f86ecb71aca7be45476bd9e58cddf
leo <leorod18@yahoo.com>
2025-09-11 20:50:51 -0400

    wrong info style

--- project-2/leo/index.js
+++ project-2/leo/index.js
@@ -1,9 +1,5 @@
 // ----------------
 const loginBtn = document.getElementById("login-btn");
-
-const passwordId = document.getElementById("password");
-const passwordValue = passwordId.value; // THIS LINE ADD VALUE
-// const changePasswordRed = passwordId.style.borderColor = "red"
 const messageEl = document.getElementById("message");
 // -----------------
 const LOGIN_USERNAME = "leo";
@@ -12,14 +8,21 @@ const LOGIN_PASSWORD = "123"
 const login = () => {
   const usernameId = document.getElementById("username");
   const usernameInput = usernameId.value;
+  const passwordId = document.getElementById("password");
+  const passwordInput = passwordId.value;
 
-  // LOGIN WHEN EMPTY
-  if (usernameInput == "") {
+  // IF USERNAME INPUT IS EMPTY OR PASSWORD INPUT IS EMPTY
+  if (usernameInput == "" || passwordInput == "") {
     alert("Field cannot be empty!");
   }
-  // LOGIN WHEN WRONG INFO
-  else if (usernameInput !== LOGIN_USERNAME) {
-    const changeUsernameRed = usernameId.style.borderColor = "red"
+  // IF USERNAME INPUT IS WRONG OR PASSWORD INPUT IS WRONG
+  else if (usernameInput !== LOGIN_USERNAME || passwordInput !== LOGIN_PASSWORD) {
+    if (usernameInput !== LOGIN_USERNAME) {
+      const changeUsernameRed = usernameId.style.borderColor = "red"
+    }
+    if (passwordInput !== LOGIN_PASSWORD) {
+      const changePasswordRed = passwordId.style.borderColor = "red"
+    }
   }
   // LOGIN WHEN CORRECT INFO
   else if (usernameInput == LOGIN_USERNAME) {
@@ -28,5 +31,5 @@ const login = () => {
 
   console.log(usernameInput);
 }
- 
+
 loginBtn.addEventListener("click", login)
