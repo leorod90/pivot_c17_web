1256cd09c8be57a63165954e58117b30ad83a626
chorn123 <hornechristal@gmail.com>
2025-10-06 18:49:07 -0500

    dom

---
+++ project-2-clicker/Christal/login.js
@@ -0,0 +1,7 @@
+const emailEl = document.getElementById("email");
+const passwordEl = document.getElementById("password");
+const loginEl =document.getElementById("login-butn");
+
+document.getElementById("login-btn").addEventListener("click", function (){
+    
+}
