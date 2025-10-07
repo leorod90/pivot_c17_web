d1e003ab78bc8f476df6954f79d082c1970e8937
Jacqueline <jackiewicker82@gmail.com>
2025-10-07 18:19:06 -0500

    javascript done

---
+++ project-3-DOM/jackie/index.js
@@ -0,0 +1,40 @@
+console.log("Hello");
+
+const color = "red";
+// color = "blue";
+
+let number = 9;
+
+function functionName(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+function sayHello(placeholde) {
+  console.log("Hello, "name+ "!");
+}
+
+sayHello("Leo")
+sayHello("Jacqueline")
+
+// Create a function called addTwo
+function addTwo(placeHolderOne, placeHolderTwo) {
+  // Add the two parameters and log the result
+  console.log(placeHolderOne + placeHolderTwo);
+}
+
+// Call the function two times
+addTwo(5, 3);   // console.log(5+3)
+addTwo(10, 7);  // console.log(10+7)
+
+const personOne = {
+    name: "leo",
+    age: 35,
+   
+}
+
+function printPerson(object){
+  console.log(object.name + " is " + personOne.age + " years old.")
+  console.log(object.name + " is " + personTwo.age + " years old.")
+}
+
+printPerson(personOne)  
+printPerson(personTwo)
