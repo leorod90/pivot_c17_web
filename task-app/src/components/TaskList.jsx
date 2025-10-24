5726cc5ff581f93def21ffae61a4cca9f8e15f8a
Jarrecia <jarreciawhite@gmail.com>
2025-10-23 20:27:15 -0500

    TaskList Done

--- task-app/src/components/TaskList.jsx
+++ task-app/src/components/TaskList.jsx
@@ -1,29 +1,50 @@
+import { useState } from "react"
+
+
 const TODO_LIST = [
     { title: "Get Milk", dueDate: "10/31/25" },
-    { title: "Get Wieners", dueDate: "13/31/25", done: true},
+    { title: "Get Wieners", dueDate: "13/31/25", },
     { title: "Get Tape", dueDate: "14/31/25" },
     { title: "Get Donkey Shoes", dueDate: "11/30/25" },
 ]
 console.log(TODO_LIST)
 // one item
 function TaskItem(props) {
+
+    const [isDone, setIsDone] = useState(true);
+
+
+    function toggleCheck() {
+if(isDone == true){
+    setIsDone(false);
+} else {
+    setIsDone(true);
+}
+       
+    }
+
     return (
         <div className="task">
-            <h3 className={props.done == true ? "done" : ""
+            <h3 className={
+                isDone == true ? "done" : ""
             }>{props.title}</h3>
-            <p>Due:{props.dueDate}</p>
-            <input type="checkbox" />
-        </div>
+            <p className={
+                isDone == true ? "done" : ""}
+            >Due:{props.dueDate}</p>
+            <input type="checkbox"
+                onChange={toggleCheck}
+                checked={isDone} />
+        </div >
     )
 }
 // list of items
 function TaskList() {
     return (
-    <h1>test</h1>
+
         <section>
             {
                 TODO_LIST.map(
-                   (props,index) => { }
+                    (props, index) => { }
                 )
             }
             <TaskItem
