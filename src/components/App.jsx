68b27a4dc3832c77c349d293886e2a61e4553802
Cotyh-code <cotyhunt222@gmail.com>
2025-12-01 06:44:42 -0600

    cotyCapstone

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, }
+from 'react-router-dom';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+  
+
+   
+     
+};
+
+export default App;
